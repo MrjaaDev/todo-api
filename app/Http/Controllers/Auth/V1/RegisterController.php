4d4b61f080a6a9db1b5379c6701c814a5a103804
Sahand Dirbaz <shdz.sha1@proton.me>
2023-11-08 21:09:41 +0330

    refactor(controller): Add user registration controller
    The first phase of the user registration controller and validation of
    incoming requests

---
+++ app/Http/Controllers/Auth/V1/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\RegisterRequest;
+
+class RegisterController extends Controller
+{
+    public function __invoke(RegisterRequest $request)
+    {
+
+    }
+}
