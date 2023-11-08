d2c165927c136caf275ec77cfc86a4023ac3ea3c
Sahand Dirbaz <shdz.sha1@proton.me>
2023-11-08 22:01:45 +0330

    feat(controller): Implementation of the login controller
    Implementation of login controller along with validation

---
+++ app/Http/Controllers/Auth/V1/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\LoginRequest;
+
+class LoginController extends Controller
+{
+    public function __invoke(LoginRequest $request)
+    {
+
+    }
+}
